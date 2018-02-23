6d754beddb99762ca9325229ba53c1b466a6c579
Alona Vereshaka <=>
2018-02-23 18:57:54 +0200

    add comment

--- Task 2/2-product-of-other-numbers.js
+++ Task 2/2-product-of-other-numbers.js
@@ -11,6 +11,7 @@ function getProductsOfAllIntsExceptAtIndex(intArray) {
 
   // for each integer, we find the product of all the integers
   // before it, storing the total product so far each time
+  // Skip first item
   let productSoFar = intArray[0];
   for (let i = 1; i < arrayLen; i++) {
     productsOfAllIntsExceptAtIndex.push(productSoFar);
@@ -21,6 +22,7 @@ function getProductsOfAllIntsExceptAtIndex(intArray) {
   // after it. since each index in products already has the
   // product of all the integers before it, now we're storing
   // the total product of all other integers
+  // Skip last item
   productSoFar = intArray[arrayLen - 1];
   for (let j = arrayLen - 2; j >= 0; j--) {
     productsOfAllIntsExceptAtIndex[j] *= productSoFar;
