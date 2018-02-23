ba50ba83eeaff78a89f58fdd0448b3dab457984e
Alona Vereshaka <=>
2018-02-23 19:49:05 +0200

    add codes

--- Task-4.js
+++ Task-4.js
@@ -16,3 +16,43 @@
     Proper tooling + bundling setup with Webpack
     + So much more
  */
+
+/**
+ * https://www.interviewcake.com/question/javascript/js-scope
+ */
+
+var text = 'outside';
+function logIt() {
+  console.log(text); // [Console] - "undefined"
+  let text = 'inside'; // if we comment this line then [Console] - "outside"
+};
+logIt();
+
+/**
+ * https://www.interviewcake.com/question/javascript/permutation-palindrome
+ */
+
+var test = ['civic', 'ivicc', 'civil', 'livci']
+
+function permutationPalindrome(str) {
+  const len = str.length;
+  let reputation = {};
+  
+  for (let i = 0; i < len; i++) {
+    reputation[str[i]] = reputation[str[i]] ? reputation[str[i]] + 1 : 1;
+  }
+
+  let oddCount = 0;
+  for (let key in reputation) {
+    if (reputation[key] % 2 === 1) {
+      oddCount ++;
+      if (oddCount > 2) return false;
+    }
+  }
+  
+  return true;
+}
+
+for (let i = 0; i < test.length; i++) {
+  console.log(test[i] + ':' + permutationPalindrome(test[i]));
+}
