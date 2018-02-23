caeccaee7a2d54cec0810f54c4b142a0df7ce34f
Alona Vereshaka <=>
2018-02-23 19:27:23 +0200

    update for task 3,4

---
+++ Task-3.js
@@ -0,0 +1,4 @@
+/**
+ * Yes.
+ * Reference for easier understanding : https://rob-bell.net/2009/06/a-beginners-guide-to-big-o-notation/
+ */
---
+++ Task-4.js
@@ -0,0 +1,18 @@
+/**
+ * [Experience] : 4 years
+ * [Suggestion] : We should use ES6 features for our JavaScript development.
+ */
+
+/**
+ * ES6 let you get JavaScript skills while preparing and updating you to write modern JavaScript.
+
+    ES6 Easy Wins - New Variables, Scoping and Template Strings
+    New Language Features — Arrow Functions, Destructuring
+    New Iterables and Looping over data
+    What the heck those ...three ...little ...dots actually do
+    How to structure your JavaScript with Modules and NPM
+    Advanced Classes, Proxies, Generators, Sets and Maps
+    How to write clean and consistent JavaScript with ESLint
+    Proper tooling + bundling setup with Webpack
+    + So much more
+ */
