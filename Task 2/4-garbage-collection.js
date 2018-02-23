98a7966edd41b7de59321d06b147c0bace3b0fa3
Alona Vereshaka <=>
2018-02-23 19:09:18 +0200

    GC

---
+++ Task 2/4-garbage-collection.js
@@ -0,0 +1,6 @@
+/**
+ * Garbage Collection is called GC, and it is woefully out of date and should be ignored completely.
+ * But we should inform visitors to know about GC is working in the background of JavaScript.
+ * Even if we don't know about GC, but developers are using JavaScript.
+ * If we know well about GC, then we can write JavaScript code better.
+ */
