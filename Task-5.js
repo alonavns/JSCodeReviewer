06d20e3f02d23af2515e9e2608f65ce80eba3287
Alona Vereshaka <=>
2018-02-23 20:57:17 +0200

    task 5

---
+++ Task-5.js
@@ -0,0 +1,19 @@
+// wrong or missing possessive apostrophe
+// misspelling
+// "me and her" instead of "her and me"
+// "your" instead of "you're" (or other way around)
+// "quicker" when it should be "more quickly" (adjective vs adverb)
+// wrong adjective order "red big car" instead of "big red car"
+
+/**
+ * Bakeries
+ * 
+ * A bakery with a niche market, such as a cupcake bakery, might more focus on its ability to do
+ * custom cupcake designs for special occasions and include related statistics to a general interest
+ * growth in cupcakes if its the first cupcake-focused bakery in the area.
+ * If you are bakery tragets a particularly high-income clientele, your concept statement might
+ * discuss how your location makes it convenient for this clientele to reach you night and day,
+ * as well as how your quality ingredients and baking experience justify the more highly costs.
+ * The concept general statement go on your business plan, but you can also use this statement,
+ * or a modified version of it, in press releases about you are bakery and in your other marketing efforts.
+ */
